42f5c125dac1f6ab7b921599173e63e640812c40
FaysalShuvo <shuvonma57@gmail.com>
2021-09-13 10:36:12 +0600

    view engine setup done

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("a")
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// app.listen(6000);
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
